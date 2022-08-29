ee36c73f63493971149c101cf9566d6099096a6f
simon3073 <simonbertoli@gmail.com>
2022-08-29 12:45:42 +1000

    Added Enter form functionality

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,132 +1,183 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Form, Button, Card, Container, Modal } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Form, Button, Card, Container, Modal } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 // connect to the redux actions
-import { connect } from 'react-redux';
-import { setUser } from '../../actions/actions';
+import { connect } from 'react-redux'
+import { setUser } from '../../actions/actions'
 
 // Import styles for this view
-import './login.scss';
+import './login.scss'
 
 // import logo image
-import logo from '../../img/site_logo.png';
+import logo from '../../img/site_logo.png'
 
 function LoginView(props) {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-	const [usernameErr, setUsernameErr] = useState('');
-	const [passwordErr, setPasswordErr] = useState('');
-	const [modalView, setModalView] = useState(false);
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [usernameErr, setUsernameErr] = useState('')
+  const [passwordErr, setPasswordErr] = useState('')
+  const [modalView, setModalView] = useState(false)
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		if (validate()) {
-			// if there are no client side registration errors
-			// process the form
-			try {
-				const response = await axios.post('https://movie-app-3073.herokuapp.com/login', {
-					Username: username,
-					Password: password
-				});
+  //   useEffect(() => {
+  //     const listener = (event) => {
+  //       if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+  //         console.log('Enter Pressed', username, password)
+  //         handleSubmit(event)
+  //       }
+  //     }
+  //     document.addEventListener('keydown', listener)
+  //     return () => {
+  //       document.removeEventListener('keydown', listener)
+  //     }
+  //   }, [])
 
-				// set the user property in redux store
-				props.setUser(response.data.user.Username);
-				localStorage.setItem('token', response.data.token);
-				localStorage.setItem('user', response.data.user.Username);
-				// Log in to the app
-				props.onLoggedIn();
-			} catch (error) {
-				setModalView(true);
-				console.log('User not in system', error);
-			}
-		}
-	};
+  const handleSubmit = async (e) => {
+    console.log(
+      '🚀 ~ file: login-view.jsx ~ line 62 ~ handleSubmit ~ handleSubmit',
+      username,
+      password
+    )
 
-	// Validate the forms input on submit to add extra layer of code validation and client advice
-	const validate = () => {
-		let isReq = true;
-		// reset error checking values
-		setUsernameErr('');
-		setPasswordErr('');
-		// check username existence and length
-		if (!username) {
-			setUsernameErr('Username Required');
-			isReq = false;
-		} else if (username.length < 4) {
-			setUsernameErr('Username must be at least 4 characters long');
-			isReq = false;
-		}
-		// check password existence and length
-		if (!password) {
-			setPasswordErr('Password Required');
-			isReq = false;
-		} else if (password.length < 6) {
-			setPasswordErr('Password must be at least 6 characters long');
-			isReq = false;
-		}
-		return isReq;
-	};
+    e.preventDefault()
+    if (validate()) {
+      // if there are no client side registration errors
+      // process the form
+      try {
+        const response = await axios.post(
+          'https://movie-app-3073.herokuapp.com/login',
+          {
+            Username: username,
+            Password: password,
+          }
+        )
 
-	// Functions needed to open and close the modal (below) to delete a user
-	const modalClose = () => setModalView(false);
+        // set the user property in redux store
+        props.setUser(response.data.user.Username)
+        localStorage.setItem('token', response.data.token)
+        localStorage.setItem('user', response.data.user.Username)
+        // Log in to the app
+        props.onLoggedIn()
+      } catch (error) {
+        setModalView(true)
+        console.log('User not in system', error)
+      }
+    }
+  }
 
-	// Function that contains the modal to delete a users account
-	const noUserModal = () => {
-		return (
-			<>
-				<Modal show={modalView} centered className="modal-display modal-md">
-					<Modal.Header>
-						<Modal.Title>Invalid Username or Password</Modal.Title>
-					</Modal.Header>
-					<Modal.Body>The username or password were entered incorrectly, or there is not an account associated with these details.</Modal.Body>
-					<Modal.Footer>
-						<Button variant="primary" onClick={modalClose}>
-							OK
-						</Button>
-					</Modal.Footer>
-				</Modal>
-			</>
-		);
-	};
+  // Validate the forms input on submit to add extra layer of code validation and client advice
+  const validate = () => {
+    console.log('Validate - ', username, password)
+    let isReq = true
+    // reset error checking values
+    setUsernameErr('')
+    setPasswordErr('')
+    // check username existence and length
+    if (!username) {
+      setUsernameErr('Username Required')
+      isReq = false
+    } else if (username.length < 4) {
+      setUsernameErr('Username must be at least 4 characters long')
+      isReq = false
+    }
+    // check password existence and length
+    if (!password) {
+      setPasswordErr('Password Required')
+      isReq = false
+    } else if (password.length < 6) {
+      setPasswordErr('Password must be at least 6 characters long')
+      isReq = false
+    }
+    return isReq
+  }
 
-	return (
-		<Container fluid className="login-container d-flex flex-column justify-content-center align-items-center">
-			<Card className="login-card">
-				<div className="m-4 text-center">
-					<img src={logo} />
-				</div>
-				<Card.Body>
-					<h2 className="mb-2 text-center">
-						<strong>Login</strong>
-					</h2>
-					<Form className="login-form">
-						<Form.Group controlId="username">
-							<Form.Label>Username:</Form.Label>
-							<Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your username" required />
-							{usernameErr && <p className="error-msg">{usernameErr}</p>}
-						</Form.Group>
-						<Form.Group controlId="password">
-							<Form.Label>Password:</Form.Label>
-							<Form.Control type="password" value={password} minLength="8" placeholder="Your Password must be 8 or more characters" onChange={(e) => setPassword(e.target.value)} required />
-						</Form.Group>
-						{passwordErr && <p className="error-msg">{passwordErr}</p>}
-						<Button variant="primary" className="btn-block mt-5" type="submit" onClick={handleSubmit}>
-							Log In
-						</Button>
-						<Link to={'/register'}>
-							<Button variant="link" className="btn-block text-white mt-3" type="link">
-								Create an an account
-							</Button>
-						</Link>
-					</Form>
-				</Card.Body>
-			</Card>
-			{noUserModal()}
-		</Container>
-	);
+  // Functions needed to open and close the modal (below) to delete a user
+  const modalClose = () => setModalView(false)
+
+  // Function that contains the modal to delete a users account
+  const noUserModal = () => {
+    return (
+      <>
+        <Modal show={modalView} centered className="modal-display modal-md">
+          <Modal.Header>
+            <Modal.Title>Invalid Username or Password</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            The username or password were entered incorrectly, or there is not
+            an account associated with these details.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={modalClose}>
+              OK
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    )
+  }
+
+  return (
+    <Container
+      fluid
+      className="login-container d-flex flex-column justify-content-center align-items-center"
+    >
+      <Card className="login-card">
+        <div className="m-4 text-center">
+          <img src={logo} />
+        </div>
+        <Card.Body>
+          <h2 className="mb-2 text-center">
+            <strong>Login</strong>
+          </h2>
+          <Form className="login-form" onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label>Username:</Form.Label>
+              <Form.Control
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Enter your username"
+                required
+              />
+              {usernameErr && <p className="error-msg">{usernameErr}</p>}
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control
+                type="password"
+                value={password}
+                minLength="8"
+                placeholder="Your Password must be 8 or more characters"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </Form.Group>
+            {passwordErr && <p className="error-msg">{passwordErr}</p>}
+            <Button
+              variant="primary"
+              className="btn-block mt-5"
+              type="submit"
+              //   onClick={handleSubmit}
+            >
+              Log In
+            </Button>
+            <Link to={'/register'}>
+              <Button
+                variant="link"
+                className="btn-block text-white mt-3"
+                type="link"
+              >
+                Create an an account
+              </Button>
+            </Link>
+          </Form>
+        </Card.Body>
+      </Card>
+      {noUserModal()}
+    </Container>
+  )
 }
 
 // 	connect to the actions
-export default connect(null, { setUser })(LoginView);
+export default connect(null, { setUser })(LoginView)
--- src/components/register-view/register-view.jsx
+++ src/components/register-view/register-view.jsx
@@ -1,148 +1,196 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Form, Button, Container, Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Form, Button, Container, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 // connect to the redux actions
-import { connect } from 'react-redux';
-import { setUser } from '../../actions/actions';
+import { connect } from 'react-redux'
+import { setUser } from '../../actions/actions'
 
 // Import styles for this view
-import './register.scss';
+import './register.scss'
 
 // import logo image
-import logo from '../../img/site_logo.png';
+import logo from '../../img/site_logo.png'
 
 // Display the registration form
 function RegisterView(props) {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-	const [email, setEmail] = useState('');
-	const [birthday, setBirthday] = useState('');
-	const [values, setValues] = useState({
-		birthdayErr: '',
-		usernameErr: '',
-		passwordErr: '',
-		emailErr: ''
-	});
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [birthday, setBirthday] = useState('')
+  const [values, setValues] = useState({
+    birthdayErr: '',
+    usernameErr: '',
+    passwordErr: '',
+    emailErr: '',
+  })
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		if (validate()) {
-			// if there are no client side registration errors
-			// process the form
-			try {
-				const response = await axios.post('https://movie-app-3073.herokuapp.com/register', {
-					Username: username,
-					Password: password,
-					Email: email,
-					Birthday: birthday
-				});
-				props.setUser(response.data.user.Username);
-				console.log('🚀 ~ file: register-view.jsx ~ line 42 ~ handleSubmit ~ response.data', response.data);
-				localStorage.setItem('token', response.data.token);
-				localStorage.setItem('user', response.data.user.Username);
-				window.open('/', '_self');
-			} catch (error) {
-				console.log('Error registering the user', error);
-			}
-		}
-	};
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (validate()) {
+      // if there are no client side registration errors
+      // process the form
+      try {
+        const response = await axios.post(
+          'https://movie-app-3073.herokuapp.com/register',
+          {
+            Username: username,
+            Password: password,
+            Email: email,
+            Birthday: birthday,
+          }
+        )
+        props.setUser(response.data.user.Username)
+        console.log(
+          '🚀 ~ file: register-view.jsx ~ line 42 ~ handleSubmit ~ response.data',
+          response.data
+        )
+        localStorage.setItem('token', response.data.token)
+        localStorage.setItem('user', response.data.user.Username)
+        window.open('/', '_self')
+      } catch (error) {
+        console.log('Error registering the user', error)
+      }
+    }
+  }
 
-	// function to set error messages
-	const setErrMessage = (obj) => {
-		setValues((prev) => ({ ...prev, ...obj }));
-	};
+  // function to set error messages
+  const setErrMessage = (obj) => {
+    setValues((prev) => ({ ...prev, ...obj }))
+  }
 
-	const validate = () => {
-		let isReq = true;
-		// reset error checking values
-		setValues({
-			birthdayErr: '',
-			usernameErr: '',
-			passwordErr: '',
-			emailErr: ''
-		});
-		// check username existence and length
-		if (!username) {
-			setErrMessage({ usernameErr: 'Username Required' });
-			isReq = false;
-		} else if (username.length < 4) {
-			setErrMessage({ usernameErr: 'Username must be at least 4 characters long' });
-			isReq = false;
-		}
-		// check password existence and length
-		if (!password) {
-			setErrMessage({ passwordErr: 'Password Required' });
-			isReq = false;
-		} else if (password.length < 6) {
-			setErrMessage({ passwordErr: 'Password must be at least 6 characters long' });
-			isReq = false;
-		}
-		// check email existence and format
-		const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-		if (!email) {
-			setErrMessage({ emailErr: 'Email Required' });
-			isReq = false;
-		} else if (!email.match(mailFormat)) {
-			setErrMessage({ emailErr: 'You have an invalid email address' });
-			isReq = false;
-		}
-		// check birthday existence and format
-		if (!birthday) {
-			setErrMessage({ birthdayErr: 'Birthday Required' });
-			isReq = false;
-		}
+  const validate = () => {
+    let isReq = true
+    // reset error checking values
+    setValues({
+      birthdayErr: '',
+      usernameErr: '',
+      passwordErr: '',
+      emailErr: '',
+    })
+    // check username existence and length
+    if (!username) {
+      setErrMessage({ usernameErr: 'Username Required' })
+      isReq = false
+    } else if (username.length < 4) {
+      setErrMessage({
+        usernameErr: 'Username must be at least 4 characters long',
+      })
+      isReq = false
+    }
+    // check password existence and length
+    if (!password) {
+      setErrMessage({ passwordErr: 'Password Required' })
+      isReq = false
+    } else if (password.length < 6) {
+      setErrMessage({
+        passwordErr: 'Password must be at least 6 characters long',
+      })
+      isReq = false
+    }
+    // check email existence and format
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if (!email) {
+      setErrMessage({ emailErr: 'Email Required' })
+      isReq = false
+    } else if (!email.match(mailFormat)) {
+      setErrMessage({ emailErr: 'You have an invalid email address' })
+      isReq = false
+    }
+    // check birthday existence and format
+    if (!birthday) {
+      setErrMessage({ birthdayErr: 'Birthday Required' })
+      isReq = false
+    }
 
-		return isReq;
-	};
+    return isReq
+  }
 
-	return (
-		<Container fluid className="register-container d-flex justify-content-center align-items-center">
-			<Card className="register-card">
-				<div className="m-4 text-center">
-					<img src={logo} />
-				</div>
+  return (
+    <Container
+      fluid
+      className="register-container d-flex justify-content-center align-items-center"
+    >
+      <Card className="register-card">
+        <div className="m-4 text-center">
+          <img src={logo} />
+        </div>
 
-				<Card.Body>
-					<h2 className="mb-2 text-center" style={{ color: '#ffbd24' }}>
-						<strong>Create An Account</strong>
-					</h2>
-					<Form className="reg-form">
-						<Form.Group controlId="username">
-							<Form.Label>Username:</Form.Label>
-							<Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter a username" required />
-							{values.usernameErr && <p className="error-msg">{values.usernameErr}</p>}
-						</Form.Group>
-						<Form.Group controlId="password">
-							<Form.Label>Password:</Form.Label>
-							<Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter a password" required />
-							{values.passwordErr && <p className="error-msg">{values.passwordErr}</p>}
-						</Form.Group>
-						<Form.Group controlId="email">
-							<Form.Label>Your email:</Form.Label>
-							<Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter an email address" required />
-							{values.emailErr && <p className="error-msg">{values.emailErr}</p>}
-						</Form.Group>
-						<Form.Group controlId="dob">
-							<Form.Label>Enter your Birth Date</Form.Label>
-							<Form.Control type="date" value={birthday} onChange={(e) => setBirthday(e.target.value)} placeholder="Date of Birth" />
-							{values.birthdayErr && <p className="error-msg">{values.birthdayErr}</p>}
-						</Form.Group>
-						<Button variant="primary" className="btn-block mt-5" type="submit" onClick={handleSubmit}>
-							Sign Up
-						</Button>
-						<Link to={'/'}>
-							<Button variant="link" className="btn-block text-white mt-3" type="link">
-								I already have an account
-							</Button>
-						</Link>
-					</Form>
-				</Card.Body>
-			</Card>
-		</Container>
-	);
+        <Card.Body>
+          <h2 className="mb-2 text-center" style={{ color: '#ffbd24' }}>
+            <strong>Create An Account</strong>
+          </h2>
+          <Form className="reg-form" onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label>Username:</Form.Label>
+              <Form.Control
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Enter a username"
+                required
+              />
+              {values.usernameErr && (
+                <p className="error-msg">{values.usernameErr}</p>
+              )}
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter a password"
+                required
+              />
+              {values.passwordErr && (
+                <p className="error-msg">{values.passwordErr}</p>
+              )}
+            </Form.Group>
+            <Form.Group controlId="email">
+              <Form.Label>Your email:</Form.Label>
+              <Form.Control
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter an email address"
+                required
+              />
+              {values.emailErr && (
+                <p className="error-msg">{values.emailErr}</p>
+              )}
+            </Form.Group>
+            <Form.Group controlId="dob">
+              <Form.Label>Enter your Birth Date</Form.Label>
+              <Form.Control
+                type="date"
+                value={birthday}
+                onChange={(e) => setBirthday(e.target.value)}
+                placeholder="Date of Birth"
+              />
+              {values.birthdayErr && (
+                <p className="error-msg">{values.birthdayErr}</p>
+              )}
+            </Form.Group>
+            <Button variant="primary" className="btn-block mt-5" type="submit">
+              Sign Up
+            </Button>
+            <Link to={'/'}>
+              <Button
+                variant="link"
+                className="btn-block text-white mt-3"
+                type="link"
+              >
+                I already have an account
+              </Button>
+            </Link>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  )
 }
 
 // 	connect to the actions
-export default connect(null, { setUser })(RegisterView);
+export default connect(null, { setUser })(RegisterView)
